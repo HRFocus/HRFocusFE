38752e40c436335846d7d88ab8574c933b4d8d84
Al Fareed <76606638+Al-Fareed@users.noreply.github.com>
2024-01-28 23:19:32 +0530

    changed the google button to sign up button

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./Login.css";
+import { useHistory } from 'react-router-dom';
 import loginImage from "../../utils/Images/loginPageImg.png";
 import logo from "../../utils/Images/logo.png";
 import Input from "../../shared/FormElements/Input/Input";
 import Buttons from "../../shared/FormElements/Buttons/Buttons";
 const Login = () => {
+  const history = useHistory();
+  const signUpButtonHandler = () =>{
+    history.push('/new')
+  };
   return (
     <div className="login-container">
       <div className="form-section">
@@ -33,16 +38,18 @@ const Login = () => {
             borderRadius="50px"
             marginLeft="-0.1rem"
           >
-            Sign up
+            Sign In
           </Buttons>
+          <hr/>
           <Buttons
             backgroundColor="white"
             width="100%"
             height="3rem"
             borderRadius="50px"
             marginLeft="-0.1rem"
+            onClick={signUpButtonHandler}
           >
-            Continue with Google
+            Sign Up
           </Buttons>
         </form>
       </div>
